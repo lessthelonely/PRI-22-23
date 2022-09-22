2656804db92d606e46a837ba012c97c9319c047d
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-09-22 15:52:10 +0100

    Amazon webscraping

--- amazon.py
+++ amazon.py
@@ -3,11 +3,24 @@ from urllib.request import urlopen
 import requests
 import re #to work with regular expressions
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-}
+#headers = {
+ #   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+#}
 
-page = requests.get("https://www.amazon.com/dp/B000F83SZQ", headers = headers)
+#page = requests.get("https://www.amazon.com/dp/B000F83SZQ", headers = headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(page.content)
+#soup = BeautifulSoup(page.content, 'html.parser')
+#print(page.text)
+
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+
+URL = "https://www.amazon.com/dp/B000F83SZQ"
+webpage = requests.get(URL, headers=HEADERS)
+soup = BeautifulSoup(webpage.content, "lxml")
+
+# Outer Tag Object
+title = soup.find("span", attrs={"id":'productTitle'})
+print(title.string)
+
