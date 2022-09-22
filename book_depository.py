0017e3ee1cde98fa25a78670019cd71b0b7ec8c1
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-09-22 10:44:30 +0100

    Changed file name + added checks

--- book_depository.py
+++ book_depository.py
@@ -10,7 +10,7 @@ I think the actual book title is just Verity but who knows what the title of thi
 When we use the search bar, there's an intermediate link...We can search only using the ISBN so perhaps there's an url with only the ISBN which we have and it's unique'''
 
 #intermediate url formula (searchTerm= to the ISBN13):
-url ="https://www.bookdepository.com/search?searchTerm=9780593358573&search=Find+book"
+url ="https://www.bookdepository.com/search?searchTerm=9780029141809 &search=Find+book"
 
 #url formula: 
 #url = "https://www.bookdepository.com/Verity-Colleen-Hoover/9781408726600?ref=grid-view"
@@ -18,9 +18,13 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html5lib')
 divs = soup.find_all('div')
 
-ratings = soup.find_all('span',{'class':'list-price'})  
-  
-print(ratings[0].get_text())
+#Using sale-price we will get the normal price if the book isn't on sale and the sale price if the book is on sale
+ratings = soup.find_all('span',{'class':'sale-price'})  
+if(len(ratings)):
+    print(ratings[0].get_text())
+else:
+    print("NA")    
+    
 '''page = urlopen(url) #opens the url
 html = page.read().decode("utf-8") #reads html
 soup = BeautifulSoup(html, "html.parser") #Creates a BeautifulSoup object and assigns it to the soup variable
