e9d4d49c3d805c8958eeb14e5f19d3934ff21799
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-09-24 09:45:46 +0100

    Forgot to push the changes to amazon script

--- amazon.py
+++ amazon.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import re #to work with regular expressions
+import pandas
 
 #headers = {
  #   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
@@ -12,15 +13,27 @@ import re #to work with regular expressions
 #soup = BeautifulSoup(page.content, 'html.parser')
 #print(page.text)
 
+database=pandas.read_csv('data/kindle_reviews.csv').dropna()
+
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-URL = "https://www.amazon.com/dp/B000F83SZQ"
-webpage = requests.get(URL, headers=HEADERS)
-soup = BeautifulSoup(webpage.content, "lxml")
+def getTitles(asin):
+    try:
+        URL = "https://www.amazon.com/dp/"+asin
+        webpage = requests.get(URL, headers=HEADERS)
+        soup = BeautifulSoup(webpage.content, "lxml")
 
-# Outer Tag Object
-title = soup.find("span", attrs={"id":'productTitle'})
-print(title.string)
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+        database.loc[database.asin==asin,'title'] =title
+        print(title.string)
+
+    except:
+        return False
+    return True     
+    
+database[database['asin'].apply(getTitles)]
+database.to_csv('data/kindle_reviews_with_titles.csv',encoding='utf-8')
 
