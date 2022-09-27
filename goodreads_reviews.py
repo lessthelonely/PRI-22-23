745833001f5f8423dab0b93d4eee211e14461113
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-09-27 18:25:12 +0100

    Script with reviews storing in csv

--- goodreads_reviews.py
+++ goodreads_reviews.py
@@ -4,25 +4,27 @@ import urllib.request
 import re #to work with regular expressions
 import pandas
 
-#database = pandas.read_csv('data/goodreads_with_prices.csv').dropna()
-url="https://www.goodreads.com/book/show/1001053.Between_Two_Fire"
+database = pandas.read_csv('data/goodreads_with_prices.csv').dropna()
+data=[]
 
 def getReviews(url):
     try:
         response = urllib.request.urlopen(url)
         soup = BeautifulSoup(response, 'html.parser')
-        review = []
         for i in soup.find_all('span',{'class':'readable'}):
             spans=i.find_all('span')
-            print(spans[-1].get_text(separator="\n"))
-            review.append(spans[-1].text)
+            review=spans[-1].get_text(separator="\n")
+            reviews=[]
+            reviews+=[url]
+            reviews+=[review]
+            data.append(reviews)    
     except:
         return False
     return True      
 
-getReviews(url)  
 
-#database[database['link'].apply(getLanguage)]
-#database.to_csv('data/goodreads_with_language.csv',encoding='utf-8')
+database[database['link'].apply(getReviews)]
+df=pandas.DataFrame(data,columns=['url','review'])
+df.to_csv('data/goodreads_with_review.csv',encoding='utf-8')
 
 
