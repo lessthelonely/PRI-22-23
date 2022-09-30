dc4ae16a3417ed3f263d0fa3c5b2a24af393223b
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-09-30 11:58:03 +0100

    Multithreading to get reviews

--- goodreads_reviews_multithread.py
+++ goodreads_reviews_multithread.py
@@ -3,28 +3,37 @@ from urllib.request import urlopen
 import urllib.request
 import re #to work with regular expressions
 import pandas
- 
+from concurrent.futures import ThreadPoolExecutor
+
 database = pandas.read_csv('data/goodreads_with_prices.csv').dropna()
 database = database.iloc[: , 1:]
+links=database['link'].to_list()
 data=[]
 
 def getReviews(url):
+    reviews=[]
     try:
         response = urllib.request.urlopen(url)
         soup = BeautifulSoup(response, 'html.parser')
         for i in soup.find_all('span',{'class':'readable'}):
             spans=i.find_all('span')
+            r=[]
             review=spans[-1].get_text(separator="\n")
-            reviews=[]
-            reviews+=[url]
-            reviews+=[review]
+            r+=[url]
+            r+=[review]
             print(review)
-            data.append(reviews)    
+            data.append(r)    
     except:
         return False
-    return True      
+    return reviews      
 
-database[database['link'].apply(getReviews)]
+def main():
+    with ThreadPoolExecutor(max_workers=100) as p:
+        p.map(getReviews,links)
+
+main()
+if(False in data):
+    data.remove(False)
 
 df=pandas.DataFrame(data,columns=['url','review'])
 df.to_csv('data/goodreads_with_review.csv',encoding='utf-8')
