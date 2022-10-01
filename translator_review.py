11d1ce8fd4a0c7faba376c44be281e02ef900452
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-10-01 12:14:28 +0100

    Fixed translation problem

--- translator_review.py
+++ translator_review.py
@@ -5,12 +5,13 @@ import pandas as pd
 df = pd.read_csv('data/goodreads_with_review.csv')
 
 for index in df.index:
-    lang = detect(df.loc[index,'review'])
-    if (lang != "en"):
+    if(not df.loc[index,'review'].isdigit()):
+        lang = detect(df.loc[index,'review'])
+        if (lang != "en"):
         # check if it really isn't english
-        translated = GoogleTranslator(source='auto', target='en').translate(df.loc[index,'review']) # later check if language != english and translate only then
-        df.loc[index,'review'] = translated
-        print(str(index + 2) + " " + translated)
+          translated = GoogleTranslator(source='auto', target='en').translate(df.loc[index,'review']) # later check if language != english and translate only then
+          df.loc[index,'review'] = translated
+          print(str(index + 2) + " " + translated)
 
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')] #delete Unnamed columns pandas
 df.to_csv('data/goodreads_with_review.csv')
