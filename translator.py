727c3998b73229e23d279f94740aab1761c1660d
Frederico Rodrigues <jfredrodrigues@gmail.com>
2022-10-01 01:25:11 +0100

    translator.py and translated titles csv

--- translator.py
+++ translator.py
@@ -1,16 +1,15 @@
 from deep_translator import GoogleTranslator
-from langdetect import detect
 import pandas as pd
 
-df = pd.read_csv('test/goodreads_with_prices.csv')
+df = pd.read_csv('data/goodreads_with_language.csv')
 # print(df.to_string())
 
 for index in df.index:
     #print(index)
-    lang = detect(df.loc[index,'title'])
-    if (lang != "en"):
-        # check if it really isn't english
-        translated = GoogleTranslator(source='auto', target='en').translate(df.loc[index,'title']) # later check if language != english and translate only then
-    #df.loc[index,'title'] = translated
-    print(str(index + 2) + " " + translated)
+    lang = df.loc[index,'language']
+    if (lang != "English" and not (df.loc[index,'title'].isnumeric())):
+        translated = GoogleTranslator(source='auto', target='en').translate(df.loc[index,'title'])
+        df.loc[index,'title'] = translated
+        print(str(index + 2) + " " + translated)
 
+df.to_csv('data/goodreads_titles_translated.csv',encoding='utf-8')
