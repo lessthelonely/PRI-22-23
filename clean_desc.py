4bd2e0e267838bb735fcecab76fac8bc91292434
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-10-02 10:26:25 +0100

    Some corrections + translate description

--- clean_desc.py
+++ clean_desc.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-df=pd.read_csv('data/goodreads_with_reviews_translated.csv')
+df=pd.read_csv('data/translate_description.csv')
 
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')] #delete Unnamed columns pandas
 
 
 data=[]
 for index in df.index:
-    print(len(df.loc[index,'review']))
-    if(len(df.loc[index,'review'])>5000):
+    print(len(df.loc[index,'desc']))
+    if(len(df.loc[index,'desc'])>5000):
         data.append(index)
 
 for i in data:
     df.drop(i)
 
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')] #delete Unnamed columns pandas
-df.to_csv('./data/cleaned_reviews.csv',encoding='utf-8')
+df.to_csv('./data/cleaned_desc.csv',encoding='utf-8')
 
