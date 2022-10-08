3f86fcd66a2ef9c4317f52ba14fb9565ce84a703
Frederico Rodrigues <jfredrodrigues@gmail.com>
2022-10-08 16:34:56 +0100

    sentiment analysis for reviews script

---
+++ nlp/sentiment_predict_pnn.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from transformers import pipeline
+
+df = pd.read_csv('../data/cleaned_reviews_sentiment.csv')
+
+classifier = pipeline("text-classification", model='cardiffnlp/twitter-roberta-base-sentiment', top_k=1)
+
+for index in df.index:
+    if (type(df.loc[index, 'review_sentiment']) != float):  # review was already evaluated
+        print("review already evaluated")
+        continue
+    to_analyse = df.loc[index, 'review']
+    try:
+        prediction = classifier(to_analyse, )
+    except:
+        continue
+    if (prediction[0][0]['label'] == "LABEL_2"):
+        print("positive" + str(index))
+        df.loc[index, 'review_sentiment'] = "positive"
+        
+    elif (prediction[0][0]['label'] == "LABEL_1"):
+        print("neutral" + str(index))
+        df.loc[index, 'review_sentiment'] = "neutral"
+        
+    else :
+        print("negative" + str(index))
+        df.loc[index, 'review_sentiment'] = "negative"
+        
+df.to_csv('../data/cleaned_reviews_sentiment_2.csv',encoding='utf-8')
+
