b17287d2d6b52fe0d52d8efb6822971f4f37dcf7
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-01 09:52:33 +0000

    Cleaned first file

--- cleaning_json.py
+++ cleaning_json.py
@@ -1,17 +1,55 @@
 import json
-import sys
 import os
-import yaml
-import re
 
-data=[]
+def clean_json(jsonFilePath):
+    data=[]
 
-if os.stat('solr/current_book_data_13_10.json').st_size != 0:
-    file= open('solr/current_book_data_13_10.json')
-    data=json.load(file)
+    if os.stat(jsonFilePath).st_size != 0:
+        file= open(jsonFilePath)
+        data=json.load(file)
 
-
-
-    with open('solr/current_book_data_25_10.json', 'w', encoding='utf8') as jsonf: 
-        jsonString = json.dumps(data, indent=4, ensure_ascii=False)
+    with open(jsonFilePath, 'w', encoding='utf8') as jsonf: 
+        jsonString = json.dumps(data, indent=4,ensure_ascii=False)
         jsonf.write(jsonString)
+        print(jsonFilePath)
+
+
+clean_json('solr/everything_part_1.json')
+clean_json('solr/everything_part_2.json')
+clean_json('solr/everything_part_3.json')
+clean_json('solr/everything_part_4.json')
+clean_json('solr/everything_part_5.json')
+clean_json('solr/everything_part_6.json')
+clean_json('solr/everything_part_7.json')
+clean_json('solr/everything_part_8.json')
+clean_json('solr/everything_part_9.json')
+clean_json('solr/everything_part_10.json')
+clean_json('solr/everything_part_11.json')
+clean_json('solr/everything_part_12.json')
+clean_json('solr/everything_part_13.json')
+clean_json('solr/everything_part_14.json')
+clean_json('solr/everything_part_15.json')
+clean_json('solr/everything_part_16.json')
+clean_json('solr/everything_part_17.json')
+clean_json('solr/everything_part_18.json')
+
+'''
+clean_json('solr/reviews_part_2.json')
+clean_json('solr/reviews_part_3.json')
+clean_json('solr/reviews_part_4.json')
+clean_json('solr/reviews_part_5.json')
+clean_json('solr/reviews_part_6.json')
+clean_json('solr/reviews_part_7.json')
+clean_json('solr/reviews_part_8.json')
+clean_json('solr/reviews_part_9.json')
+clean_json('solr/reviews_part_10.json')
+clean_json('solr/reviews_part_11.json')
+clean_json('solr/reviews_part_12.json')
+clean_json('solr/reviews_part_13.json')
+clean_json('solr/reviews_part_14.json')
+clean_json('solr/reviews_part_15.json')
+clean_json('solr/reviews_part_16.json')
+clean_json('solr/reviews_part_17.json')
+clean_json('solr/reviews_part_18.json')
+'''
+
