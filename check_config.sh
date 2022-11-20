0af6099f622d49728f3c952333ecd9196b760363
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-20 19:57:18 +0000

    Start new config settings

---
+++ check_config.sh
@@ -0,0 +1,1 @@
+curl http://localhost:8983/solr/books_schema/config/requestHandler
---
+++ config.sh
@@ -0,0 +1,15 @@
+curl -X POST -H 'Content-type:application/json'  -d '{
+  "add-searchcomponent": {
+    "name": "suggest",
+    "class": "solr.SuggestComponent",
+    "suggester": {
+        "name": "mySuggester",
+        "lookupImpl": "FuzzyLookupFactory",
+        "dictionaryImpl": "DocumentDictionaryFactory",
+        "field": "title_suggest",
+        "suggestAnalyzerFieldType": "text_general",
+        "exactMatchFirst": "true",
+        "buildOnStartup": "true"
+    }
+  }
+}' http://localhost:8983/solr/books_schema/config
