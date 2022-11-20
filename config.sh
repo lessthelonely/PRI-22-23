9cc23c95ed129cd9d0b5e2586d07d0cce4da6e49
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-20 22:58:05 +0000

    Spell Check setup

--- config.sh
+++ config.sh
@@ -29,3 +29,34 @@ curl -X POST -H 'Content-type:application/json'  -d '{
         ]
   }
 }' http://localhost:8983/solr/goodreads/config
+
+curl -X POST -H 'Content-type:application/json'  -d '{
+  "update-requesthandler": {
+      "name": "/select",
+      "class": "solr.SearchHandler",
+      "defaults": {
+        "wt": "json", 
+        "indent": true,
+        "df": "search",
+        "rows": 10,
+        "spellcheck": "on",
+        "spellcheck.collate": "true"
+      },
+      "last-components": [
+        "spellcheck"
+      ]
+    }
+}' http://localhost:8983/solr/books_schema/config
+
+curl -X POST -H 'Content-type:application/json'  -d '{
+   "update-searchcomponent": {
+      "name": "spellcheck",
+      "class": "solr.SpellCheckComponent",
+      "spellchecker": {
+        "classname": "solr.IndexBasedSpellChecker",
+        "spellcheckIndexDir": "./spellchecker",
+        "field": "spell",
+        "buildOnCommit": "true"
+      }
+    }
+}' http://localhost:8983/solr/books_schema/config
