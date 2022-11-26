12926057b937018fd6f0bc0c9e9e7f9756cb3751
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-26 11:38:06 +0000

    Initial docker test

--- solr/startup.sh
+++ solr/startup.sh
@@ -79,24 +79,24 @@ curl -X POST -H 'Content-type:application/json'  -d '{
 }' http://localhost:8983/solr/books_schema/config
 
 # Populate collection
-bin/post -c books_schema everything_part_1_mood.json
-bin/post -c books_schema everything_part_2_mood.json
-bin/post -c books_schema everything_part_3_mood.json
-bin/post -c books_schema everything_part_4_mood.json
-bin/post -c books_schema everything_part_5_mood.json
-bin/post -c books_schema everything_part_6_mood.json
-bin/post -c books_schema everything_part_7_mood.json
-bin/post -c books_schema everything_part_8_mood.json
-bin/post -c books_schema everything_part_9_mood.json
-bin/post -c books_schema everything_part_10_mood.json
-bin/post -c books_schema everything_part_11_mood.json
-bin/post -c books_schema everything_part_12_mood.json
-bin/post -c books_schema everything_part_13_mood.json
-bin/post -c books_schema everything_part_14_mood.json
-bin/post -c books_schema everything_part_15_mood.json
-bin/post -c books_schema everything_part_16_mood.json
-bin/post -c books_schema everything_part_17_mood.json
-bin/post -c books_schema everything_part_18_mood.json
+bin/post -c books_schema /data/everything_part_1_mood.json
+bin/post -c books_schema /data/everything_part_2_mood.json
+bin/post -c books_schema /data/everything_part_3_mood.json
+bin/post -c books_schema /data/everything_part_4_mood.json
+bin/post -c books_schema /data/everything_part_5_mood.json
+bin/post -c books_schema /data/everything_part_6_mood.json
+bin/post -c books_schema /data/everything_part_7_mood.json
+bin/post -c books_schema /data/everything_part_8_mood.json
+bin/post -c books_schema /data/everything_part_9_mood.json
+bin/post -c books_schema /data/everything_part_10_mood.json
+bin/post -c books_schema /data/everything_part_11_mood.json
+bin/post -c books_schema /data/everything_part_12_mood.json
+bin/post -c books_schema /data/everything_part_13_mood.json
+bin/post -c books_schema /data/everything_part_14_mood.json
+bin/post -c books_schema /data/everything_part_15_mood.json
+bin/post -c books_schema /data/everything_part_16_mood.json
+bin/post -c books_schema /data/everything_part_17_mood.json
+bin/post -c books_schema /data/everything_part_18_mood.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
