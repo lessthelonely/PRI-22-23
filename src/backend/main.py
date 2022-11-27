aa87cdf9c5fbe0d22cd79299c6cb2fd1a76bd607
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-27 19:15:46 +0000

    Get books by id

--- src/backend/main.py
+++ src/backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/books", status_code=status.HTTP_200_OK)
+@app.get("/books", response_model = List[Book], status_code=status.HTTP_200_OK)
 async def get_books():
     query = 'http://localhost:8983/solr/books_schema/query?q=*:*&q.op=OR&indent=true&wt=json'
     list_books= requests.get(query).json()['response']['docs']
@@ -30,6 +30,14 @@ async def get_books():
     for book in list_books:
         books.append(Book(**book))
     return books
+
+@app.get("/books/{book_id}", response_model = Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    query = 'http://localhost:8983/solr/books_schema/query?q=id:' + str(book_id) + '&q.op=OR&indent=true&qt='
+    book= requests.get(query).json()['response']['docs'][0]
+    
+    return book
+
 
 # pip install "fastapi[all]"
 # Example of how to run a file: uvicorn main:app --reload
