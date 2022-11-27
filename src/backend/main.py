d96da2c3832eff907f97423a31051d508ac238ab
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-27 19:42:20 +0000

    Get results based on query

--- src/backend/main.py
+++ src/backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
+# Get all the books (maybe do top 10 or something)
 @app.get("/books", response_model = List[Book], status_code=status.HTTP_200_OK)
 async def get_books():
     query = 'http://localhost:8983/solr/books_schema/query?q=*:*&q.op=OR&indent=true&wt=json'
@@ -31,6 +32,7 @@ async def get_books():
         books.append(Book(**book))
     return books
 
+# Get a book by id
 @app.get("/book/{book_id}", response_model = Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int):
     query = 'http://localhost:8983/solr/books_schema/query?q=id:' + str(book_id) + '&q.op=OR&indent=true&qt='
@@ -38,6 +40,23 @@ async def get_book(book_id: int):
     
     return book
 
+# Search for filters
+
+# Suggestions
+
+# Spellcheck
+
+# Search for similar books (maybe send the book characteristics + book list of similar books?)
+
+# Search with a query --> have to search for the query in every term
+@app.get("/search/{query}", response_model = List[Book], status_code=status.HTTP_200_OK)
+async def search_books(query: str):
+    query = 'http://localhost:8983/solr/books_schema/query?q=' + query+ '&q.op=OR&defType=edismax&indent=true&qf=author%20title%20book_format%20description%20genre%20isbn%20page_count%20rating%20review_count%20rating_count%20price%20sensitivity%20pacing%20buzzwords%20mood%20review&qt='
+    list_books= requests.get(query).json()['response']['docs']
+    books=[]
+    for book in list_books:
+        books.append(Book(**book))
+    return books
 
 # pip install "fastapi[all]"
 # Example of how to run a file: uvicorn main:app --reload
