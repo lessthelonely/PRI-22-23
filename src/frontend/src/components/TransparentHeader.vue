dfa1d422809e74b5530f37c5dcf548460e1f4855
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2022-11-28 19:18:58 +0000

    Frontend push.

---
+++ src/frontend/src/components/TransparentHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#" style="width: 100px;">
+                <img src="../assets/logo.png" style="width: 100%;">
+            </a>
+            <button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item nav-link"><a class="nav-link active" href="#about">About</a></li>
+                    <li class="nav-item nav-link"><a class="nav-link" href="#contact">contact</a></li>
+                    <li class="nav-item nav-link"><a class="nav-link active" href="#about">search</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Navbar',
+    setup() {
+        return {};
+    },
+});
+</script>
