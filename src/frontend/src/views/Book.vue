3dbaf89da28d1a8c060afc8b2516ff5f2207f53b
melisilva <melissa.silva2001.ms@gmail.com>
2022-11-28 22:12:02 +0000

    Started backend/frontend connection in book

--- src/frontend/src/views/Book.vue
+++ src/frontend/src/views/Book.vue
@@ -52,8 +52,8 @@
                             <div class="col flex-column">
                                 <h1 class="text-start" style="margin: 0px;margin-bottom: 15px;"><span
                                     style="color: rgb(0, 0, 0);">reviews</span></h1>
-                                <div v-if="reviews.length != 0">
-                                    <Review v-for="review in reviews" :key="review.id" :review="review" />
+                                <div v-if="review.length != 0">
+                                    <Review v-for="review in review" :key="review.id" :review="review" />
                                 </div>
                                 <p style="font-family: Arial;color: rgb(0,0,0);font-size: 18px;text-align: justify;" v-else> 
                                     No available reviews.
@@ -71,13 +71,31 @@
 import { defineComponent } from 'vue'
 import WhiteHeader from '../components/WhiteHeader.vue'
 import Review from '../components/Review.vue'
+import axios from 'axios'
+import router from '../router'
 
 export default defineComponent({
     name: 'Book',
     data() {
         return {
-            reviews: []
-        }
+            author: [],
+            book_format:[],
+            description:"",
+            genre:[],
+            cover_img:"",
+            title:"",
+            isbn:"",
+            page_count: 0,
+            rating:0,
+            review_count:0,
+            price:0,
+            sensitivity: [],
+            pacing:"",
+            buzzwords:[],
+            mood:[],
+            mood_percentage:[],
+            review:[]
+      }
     },
     components: {
         WhiteHeader,
@@ -86,6 +104,28 @@ export default defineComponent({
     setup() {
         return {}
     },
+    created(){
+        axios.get('http://localhost:8080/book/' + this.$route.params.id).then((res) =>{
+            console.log(res.data);
+            this.author = res.data.author
+            this.book_format = res.data.book_format
+            this.description = res.data.description
+            this.genre = res.data.genre
+            this.cover_img = res.data.cover_img
+            this.title = res.data.title
+            this.isbn = res.data.isbn
+            this.page_count = res.data.page_count
+            this.rating = res.data.rating
+            this.review_count = res.data.review_count
+            this.price = res.data.price
+            this.sensitivity = res.data.sensitivity
+            this.pacing = res.data.pacing
+            this.buzzwords = res.data.buzzwords
+            this.mood = res.data.mood
+            this.mood_percentage = res.data.mood_percentage
+            this.review = res.data.review
+        });
+    }
 })
 
 </script>
