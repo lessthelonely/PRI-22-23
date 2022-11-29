dd11b7ef55a89bc53027eaff42141f08c0ada780
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2022-11-29 10:51:58 +0000

    Buzzwords.

--- src/frontend/src/components/SearchResults.vue
+++ src/frontend/src/components/SearchResults.vue
@@ -1,33 +1,33 @@
 <template>
     <div class="card" style="color: rgb(0,0,0); width: 100%">
-        <div class="card-body"
-            style="display: flex;flex-direction: row;width: 100%;flex-wrap: nowrap;align-content: center;justify-content: flex-start;align-items: center;">
-            <div class="col" style="margin-right: 0px;">
+        <div class="card-body" style="">
+            <div class="col-12" style="margin-right: 0px;">
                 <div class="row">
-                    <div class="col">
+                    <div class="col-8" style="margin-right: 25px;">
+                        <div class="row" style="align-content: start;">
+                            <p class="flex-column justify-content-md-end" style="	font-size: 10px; margin-bottom: 15px; width: 100%; color: rgb(71, 67, 67); text-align: left;">
+                                <em>This one is [RANDOM BUZZWORDS] and X% of readers have found it
+                                    [RANDOM MOOD].</em>
 
-                        <h1
-                            style="margin-bottom: 0px; font-family: Cabin; text-transform: uppercase; text-align: left;">
-                            <em>{{ book.title }}</em>
-                        </h1>
-                        <h5 class="text-muted mb-2">{{ authors }}</h5>
-                        <p style="font-size: 14px;height: 200px;margin-bottom: 10px;width: 100%;text-align: justify;">
-                            <span style="color: rgb(30, 25, 21);">
-                                {{ book.description }}
-                                <button id="button-search" @click="bookPage">[MORE]</button>
-                            </span>
-                        </p>
-                        <p class="flex-column justify-content-md-end"
-                            style="font-size: 10px;margin-bottom: 0px;width: 100%;color: rgb(71,67,67);">
-                            <em>This one is [RANDOM BUZZWORDS] and X% of readers have found it
-                                [RANDOM MOOD].</em>
-
-                        </p>
+                            </p>
+                            <h1 style="margin-bottom: 0px; font-family: Cabin; text-transform: uppercase; text-align: left;">
+                                <em>{{ book.title }}</em>
+                            </h1>
+                            <h5 class="text-muted mb-2"
+                                style="font-family: Cabin; text-transform: uppercase; text-align: left; font-weight: 600;">
+                                {{ authors }}</h5>
+                            <p style="font-size: 18px;height: 200px;margin-bottom: 10px;width: 100%; text-align: left;">
+                                <span style="color: rgb(30, 25, 21);">
+                                    {{ book.description }}
+                                    <button id="button-search" @click="bookPage">[MORE]</button>
+                                </span>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="col" style="width: 10%;margin: 0px;margin-right: 10px;margin-left: 10px;">
-                    <img v-bind:src=book.cover_img
-                        style="width: auto;height: 300px;box-shadow: 2px 2px 9px 2px rgba(0,0,0,0.26);" />
+                    <div style="width: 30%;">
+                        <img v-bind:src=book.cover_img
+                            style="box-shadow: 2px 2px 9px 2px rgb(0 0 0 / 26%); float: right;" />
+                    </div>
                 </div>
             </div>
         </div>
